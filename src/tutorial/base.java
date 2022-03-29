1f42783091e4fceec823598c372d93f5edfb66e0
davidga2007 <davidgabuniaqa.com>
2022-03-28 21:46:08 -0400

    test

---
+++ src/tutorial/base.java
@@ -0,0 +1,30 @@
+package tutorial;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+
+		File file = new File("src", "ApiDemos-debug.apk");
+
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "David_Emulator");
+		cap.setCapability(MobileCapabilityType.APP, file.getAbsolutePath());
+
+		// UI automator -- Android apps
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+
+	}
+
+}
